26a07dc744f1fd923195ac40180ba0e8d6fd04e8
FelixBerget <felixberget@gmail.com>
2025-11-26 12:20:18 +0100

    do it

---
+++ OwnProject/OwnProject/Crocodilian.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OwnProject
+{
+    public class Crocodilian : Lizard
+    {
+
+        public string getName()
+        {
+            return name;
+        }
+        public string getDescription()
+        {
+            return description;
+        }
+        public string getType()
+        {
+            return type;
+        }
+        public string getLength()
+        {
+            return length;
+        }
+        public string getWeight()
+        {
+            return weight;
+        }
+    }
+}
---
+++ OwnProject/OwnProject/Lizard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OwnProject
+{
+    public class Lizard
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string type { get; set; }
+        public string length { get; set; }
+        public string weight { get; set; }
+    }
+}
