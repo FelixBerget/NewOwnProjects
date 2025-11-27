23d682b872b26ed4bf1a84af5051350aa77d4212
FelixBerget <felixberget@gmail.com>
2025-11-27 11:28:29 +0100

    Create Lizardinfo.cs

---
+++ OwnProject/OwnProject/Lizardinfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OwnProject
+{
+    internal class Lizardinfo
+    {
+        static List<Lizard> Lizards = new List<Lizard>();
+        public static void addDino(Dinosaur dinosaur)
+        {
+            Lizards.Add(dinosaur);
+        }
+        public static int getListLength()
+        {
+            return Lizards.Count;
+        }
+    }
+}
