996d9178cd3b799a83bafe2677744911462fa8ce
FelixBerget <felixberget@gmail.com>
2025-12-01 10:03:55 +0100

    more additions

---
+++ SelfProject/SelfProject/MyConsole.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SelfProject
+{
+    internal class MyConsole
+    {
+
+        public static string GetInfo(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
+        }
+
+        public static void ChangeColor(ConsoleColor foreColor,ConsoleColor backColor)
+        {
+            Console.ForegroundColor = foreColor;
+            Console.ForegroundColor = backColor;
+        }
+    }
+}
---
+++ SelfProject/SelfProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SelfProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
