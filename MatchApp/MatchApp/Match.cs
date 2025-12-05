2018e7d413721b44a96cd58e4c90d90ccaf25ccc
FelixBerget <felixberget@gmail.com>
2025-12-05 09:45:52 +0100

    Do task

---
+++ MatchApp/MatchApp/Match.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatchApp
+{
+    internal class Match
+    {
+
+        int homeGoals = 0;
+        int awayGoals = 0;
+        bool matchIsRunning = true;
+
+        public void StartGame()
+        {
+            Console.Write("Gyldig tips: \n" +
+" - H, U, B\n" +
+" - halvgardering: HU, HB, UB\n" +
+" - helgardering: HUB\n" +
+"Hva har du tippet for denne kampen? ");
+            var bet = Console.ReadLine();
+            while (matchIsRunning)
+            {
+                SetTheResult();
+            }
+            CalculateResult(bet);
+
+        }
+
+        public void SetTheResult()
+        {
+            Console.Write("Kommandoer: \n" +
+    " - H = scoring hjemmelag\n" +
+    " - B = scoring bortelag\n" +
+    " - X = kampen er ferdig\n" +
+    "Angi kommando: ");
+            var command = Console.ReadLine();
+            matchIsRunning = command != "X";
+            if (command == "H") homeGoals++;
+            else if (command == "B") awayGoals++;
+            Console.WriteLine($"Stillingen er {homeGoals}-{awayGoals}.");
+        }
+
+        public void CalculateResult(string bet)
+        {
+            var result = homeGoals == awayGoals ? "U" : homeGoals > awayGoals ? "H" : "B";
+            var isBetCorrect = bet.Contains(result);
+            var isBetCorrectText = isBetCorrect ? "riktig" : "feil";
+            Console.WriteLine($"Du tippet {isBetCorrectText}");
+        }
+    }
+}
---
+++ MatchApp/MatchApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MatchApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Match match = new Match();
+            match.StartGame();
+
+        }
+    }
+}
