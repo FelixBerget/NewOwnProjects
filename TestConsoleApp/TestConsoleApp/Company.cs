71196a997a8a92913deade7f23cab42a9ab4e8bc
FelixBerget <felixberget@gmail.com>
2026-01-12 14:49:17 +0100

    Update Company.cs

--- TestConsoleApp/TestConsoleApp/Company.cs
+++ TestConsoleApp/TestConsoleApp/Company.cs
@@ -9,16 +9,19 @@ namespace TestConsoleApp
         public string Name { get; private set; }
         public string Type { get; private set; }
         public Country Country { get; private set; }
+        public int Id { get; private set; }
 
         public Company(string name, string type, Country country)
         {
+            Random r  = new Random();
+            Id = r.Next(1, 1000000);
             Name = name;
             Type = type;
             Country = country;
         }
         public string ToString()
         {
-            return Name + " is a " + Type + " company from " + Country.Name; 
+            return "ID-"+ Id + " " +  Name + " is a " + Type + " company from " + Country.Name; 
         }
     }
 }
