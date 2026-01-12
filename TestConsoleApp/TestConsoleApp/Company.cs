4874fb87758beca3108c37dba103cb38ea38f2a2
FelixBerget <felixberget@gmail.com>
2026-01-12 14:12:52 +0100

    more additions

---
+++ TestConsoleApp/TestConsoleApp/Company.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConsoleApp
+{
+    public class Company
+    {
+        public string Name { get; private set; }
+        public string Type { get; private set; }
+        public Country Country { get; private set; }
+
+        public Company(string name, string type, Country country)
+        {
+            Name = name;
+            Type = type;
+            Country = country;
+        }
+        public string ToString()
+        {
+            return Name + " is a " + Type + " company from " + Country.Name; 
+        }
+    }
+}
--- TestConsoleApp/TestConsoleApp/Program.cs
+++ TestConsoleApp/TestConsoleApp/Program.cs
@@ -6,8 +6,10 @@
         {
             Country Japan = new Country("Japan", 120);
             City Tokyo = new City("Tokyo", Japan, 32);
+            Company Toyota = new Company("Toyota", "Car", Japan);
             Console.WriteLine(Japan.ToString());
             Console.WriteLine(Tokyo.ToString());
+            Console.WriteLine(Toyota.ToString());
         }
     }
 }
